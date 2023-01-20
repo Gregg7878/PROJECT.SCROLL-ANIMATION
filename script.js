a48b8ba9024a438a88fdb2b9586d0098e4dca402
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-01-20 18:09:27 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const boxes=document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkboxes)
+
+checkboxes()
+function checkboxes(){
+    const triggerButton=(window.innerHeight/5*4)
+    boxes.forEach(box=> {
+        const boxTop=box.getBoundingClientRect().top
+        if(boxTop<triggerbottom){
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+        }
+    )}
