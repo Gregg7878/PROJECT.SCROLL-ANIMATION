09d7e095ff69caa0a6bf0262fd47d2a709980cbd
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-01-20 22:37:07 +0300

    edited the script.js file

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const boxes=document.querySelectorAll('.box')
 
-window.addEventListener('scroll', checkboxes)
+window.addEventListener('scroll', checkBoxes)
 
-checkboxes()
-function checkboxes(){
-    const triggerbottom=(window.innerHeight/5*4)
+checkBoxes()
+
+function checkBoxes(){
+    const triggerBottom = window.innerHeight / 5 * 4
     boxes.forEach(box=> {
-        const boxTop=box.getBoundingClientRect().top
-        if(boxTop<triggerbottom){
+
+    const boxTop=box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
             box.classList.add('show')
-        }else{
+        } else { 
             box.classList.remove('show')
         }
         }
